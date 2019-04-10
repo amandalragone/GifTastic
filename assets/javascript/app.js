0a0f1435cf64dbe34c886fa8fdc155ba9c82bf49
Amanda <amandalragone@gmail.com>
2019-04-10 12:52:21 -0500

    Created ajax function, but it's not working yet.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
         function createButtons(topicName) {
             return `
-            <button class="searchTopic">${topicName}</button>
+            <button class="searchTopic" data-id="${topicName}">${topicName}</button>
             `
         };
 
@@ -27,6 +27,24 @@ $(document).ready(function() {
 
 
     })
+
+    
+
+    $(document).on("click", ".searchTopic", function() {
+
+        var topic = $(this).data("id");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=9VCJsnqi1AU2pxpJjc0IDYjl4enhvjWQ&q=" + topic + "&limit=10"
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+  
+            var myResponse = response;
+            $("#results-view").html(myResponse);
+          });
+
+    });
 
     displayButtons();
 
